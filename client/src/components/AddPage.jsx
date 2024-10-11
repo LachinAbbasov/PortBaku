21e2f7e805d170883e96d2af9f15ff7ee160a8b7
abbasov lacin <tu7n8gcpt@code.edu.az>
2024-10-11 09:47:43 +0400

    antd design added

--- client/src/components/AddPage.jsx
+++ client/src/components/AddPage.jsx
@@ -1,144 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
+import { Table, Input, Button, Form, Select } from 'antd';
 import { addProduct, selectProducts } from '../redux/productSlice';
 
+const { Option } = Select;
 
 const categoryProducts = {
-  Mayalı: [
-    'Sadə bulka',
-    'Kişmişli',
-    'Qarabağ kətəsi',
-    'Simit',
-    'Vatruşka',
-    'Alma cemli',
-    'Kəsmikli kökə',
-    'Şokaladlı',
-    'Albalı cemli',
-    'Qozlu',
-    'Qoğal şor',
-    'Qoğal şirin',
-    'Darçınlı bulka',
-    'Minni pizza',
-    'Hod-dog',
-    'Qaraköy',
-    'Özbək dönəri',
-    'Xaçapuri',
-    'Qatlama toyuqlu',
-    'Peraşki kartoflu',
-    'Peraşki toyuqlu',
-    'Pendirli açma',
-    'Fransız bulkası'
-  ],
-  Tort: [
-    'Yay tortu M',
-    'Yat tortu S',
-    'Birdmilk M',
-    'Birdmilk S',
-    'Honey M',
-    'Honey S',
-    'Napaleon M',
-    'Napaleon S',
-    'Banana cake M',
-    'Banana cake S',
-    'Damla şokaladlı M',
-    'Damla şokaladlı S',
-    'Kiyeviski M',
-    'Kiyeviski S',
-    'Kudruyaj M',
-    'Kudruyaj S',
-    'Blinçik M',
-    'Blinçik S',
-    'Black Forest roll',
-    'Redvel vet roll',
-    'Orange cake',
-    'Engilis cake',
-    'Skazka roll',
-    'Birdmilk desert',
-    'Honey desert',
-    'Tiramisu desert',
-    'Praqa desert',
-    'Kudruyaj desert',
-    'San Sebastian'
-  ],
-  Paxlavalar: [
-    'Qozlu paxlava',
-    'Fındıqlı paxlava',
-    'Şokaladlı paxlava',
-    'Türk paxlavaları'
-  ],
-  QuruMallar: [
-    'Şəkərbura',
-    'Acı badəm',
-    'Şəkər çörəyi',
-    'Mutaki',
-    'Qozlu kətə',
-    'Limonçik',
-    'Damla peçeniyası',
-    'Qızıl gül (kukis)'
-  ],
-  Kulinaria: [
-    'Sendiviç',
-    'Blinçik ətli',
-    'Blinçik toyuqlu',
-    'Blinçik toyuq tərəvəzli',
-    'Blinçik kəsmikli',
-    'Şumardin blinçik',
-    'Toyuq nuggets',
-    'Şnitzel',
-    'Toyuq kotleti',
-    'Toyuq çöp şiş'
-  ],
-  Salatlar: [
-    'Paytaxt salatı',
-    'Toyuq salatı mayanezli',
-    'Toyuq salatı mayanezsiz',
-    'Vinegret salatı',
-    'Gavalı salatı',
-    'Mimoza salatı',
-    'Nar salatı',
-    'Çuğundur salatı'
-  ],
-  Yeməklər: [
-    'Toyuq file',
-    'Fajitos',
-    'Zəfəran plov',
-    'Şəhriyyə plov',
-    'Qarabaşaq',
-    'Ev sayağı kartof',
-    'Sobada tərəvəz',
-    'Qril',
-    'Ləvəngi',
-    'Tabaka'
-  ],
+  Mayalı: ['Sadə bulka', 'Kişmişli', 'Qarabağ kətəsi', 'Simit', 'Vatruşka', 'Alma cemli', 'Kəsmikli kökə', 'Şokaladlı', 'Albalı cemli', 'Qozlu', 'Qoğal şor', 'Qoğal şirin', 'Darçınlı bulka', 'Minni pizza', 'Hod-dog', 'Qaraköy', 'Özbək dönəri', 'Xaçapuri', 'Qatlama toyuqlu', 'Peraşki kartoflu', 'Peraşki toyuqlu', 'Pendirli açma', 'Fransız bulkası'],
+  Tort: ['Yay tortu M', 'Yat tortu S', 'Birdmilk M', 'Birdmilk S', 'Honey M', 'Honey S', 'Napaleon M', 'Napaleon S', 'Banana cake M', 'Banana cake S', 'Damla şokaladlı M', 'Damla şokaladlı S', 'Kiyeviski M', 'Kiyeviski S', 'Kudruyaj M', 'Kudruyaj S', 'Blinçik M', 'Blinçik S', 'Black Forest roll', 'Redvel vet roll', 'Orange cake', 'Engilis cake', 'Skazka roll', 'Birdmilk desert', 'Honey desert', 'Tiramisu desert', 'Praqa desert', 'Kudruyaj desert', 'San Sebastian'],
+  Paxlavalar: ['Qozlu paxlava', 'Fındıqlı paxlava', 'Şokaladlı paxlava', 'Türk paxlavaları'],
+  QuruMallar: ['Şəkərbura', 'Acı badəm', 'Şəkər çörəyi', 'Mutaki', 'Qozlu kətə', 'Limonçik', 'Damla peçeniyası', 'Qızıl gül (kukis)'],
+  Kulinaria: ['Sendiviç', 'Blinçik ətli', 'Blinçik toyuqlu', 'Blinçik toyuq tərəvəzli', 'Blinçik kəsmikli', 'Şumardin blinçik', 'Toyuq nuggets', 'Şnitzel', 'Toyuq kotleti', 'Toyuq çöp şiş'],
+  Salatlar: ['Paytaxt salatı', 'Toyuq salatı mayanezli', 'Toyuq salatı mayanezsiz', 'Vinegret salatı', 'Gavalı salatı', 'Mimoza salatı', 'Nar salatı', 'Çuğundur salatı'],
+  Yeməklər: ['Toyuq file', 'Fajitos', 'Zəfəran plov', 'Şəhriyyə plov', 'Qarabaşaq', 'Ev sayağı kartof', 'Sobada tərəvəz', 'Qril', 'Ləvəngi', 'Tabaka'],
 };
+
 const AddPage = () => {
   const dispatch = useDispatch();
   const products = useSelector(selectProducts);
 
   const branches = ['İnqilab', 'Mərdəkan', 'Şüvalan', 'Aquapark', 'Nargilə', 'Mum', 'Buzovna', 'Bayl'];
 
-  const [selectedCategory, setSelectedCategory] = React.useState('');
-  const [selectedProduct, setSelectedProduct] = React.useState('');
-  const [branchName, setBranchName] = React.useState('');
-  const [soldQuantity, setSoldQuantity] = React.useState('');
-  const [preparedQuantity, setPreparedQuantity] = React.useState('');
-  const [unfitQuantity, setUnfitQuantity] = React.useState('');
-  const [expiredQuantity, setExpiredQuantity] = React.useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState('');
+  const [branchName, setBranchName] = useState('');
+  const [soldQuantity, setSoldQuantity] = useState('');
+  const [preparedQuantity, setPreparedQuantity] = useState('');
+  const [unfitQuantity, setUnfitQuantity] = useState('');
+  const [expiredQuantity, setExpiredQuantity] = useState('');
+  const [searchText, setSearchText] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  
+  const handleSubmit = async () => {
     const newProduct = {
       branchName,
       productName: selectedProduct,
-      category: selectedCategory, // Seçim kateqoriyası
+      category: selectedCategory,
       soldQuantity: Number(soldQuantity),
       preparedQuantity: Number(preparedQuantity),
       unfitQuantity: Number(unfitQuantity),
       expiredQuantity: Number(expiredQuantity),
     };
-  
+
     const response = await fetch('http://localhost:5000/api/mehsullar/mehsullar', {
       method: 'POST',
       headers: {
@@ -146,11 +48,10 @@ const AddPage = () => {
       },
       body: JSON.stringify(newProduct),
     });
-  
+
     if (response.ok) {
       const savedProduct = await response.json();
       dispatch(addProduct(savedProduct));
-      // State'ləri sıfırlayın
       setBranchName('');
       setSelectedCategory('');
       setSelectedProduct('');
@@ -159,136 +60,114 @@ const AddPage = () => {
       setUnfitQuantity('');
       setExpiredQuantity('');
     } else {
-      console.error('Məhsul əlavə edilərkən xəta baş verdi.');
+      console.error('Error adding product');
     }
   };
 
-  const calculateDifference = (product) => {
-    return product.preparedQuantity - (product.soldQuantity + product.unfitQuantity + product.expiredQuantity);
+  const handleSearch = (e) => {
+    setSearchText(e.target.value);
   };
+
+  const filteredProducts = products.filter((product) =>
+    product.productName.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  const columns = [
+    {
+      title: 'Filialın Adı',
+      dataIndex: 'branchName',
+      key: 'branchName',
+      sorter: (a, b) => a.branchName.localeCompare(b.branchName),
+    },
+    {
+      title: 'Məhsulun Adı',
+      dataIndex: 'productName',
+      key: 'productName',
+      sorter: (a, b) => a.productName.localeCompare(b.productName),
+    },
+    {
+      title: 'Satış Miqdarı',
+      dataIndex: 'soldQuantity',
+      key: 'soldQuantity',
+      sorter: (a, b) => a.soldQuantity - b.soldQuantity,
+    },
+    {
+      title: 'Hazırlanan Miqdar',
+      dataIndex: 'preparedQuantity',
+      key: 'preparedQuantity',
+      sorter: (a, b) => a.preparedQuantity - b.preparedQuantity,
+    },
+    {
+      title: 'Yararsız',
+      dataIndex: 'unfitQuantity',
+      key: 'unfitQuantity',
+      sorter: (a, b) => a.unfitQuantity - b.unfitQuantity,
+    },
+    {
+      title: 'Satış Tarixi Bitmiş',
+      dataIndex: 'expiredQuantity',
+      key: 'expiredQuantity',
+      sorter: (a, b) => a.expiredQuantity - b.expiredQuantity,
+    },
+    {
+      title: 'Qalıq',
+      key: 'difference',
+      render: (_, product) => product.preparedQuantity - (product.soldQuantity + product.unfitQuantity + product.expiredQuantity),
+    },
+  ];
 
   return (
     <div>
       <h1>Kontor Cədvəl</h1>
-      <form onSubmit={handleSubmit} style={{ marginBottom: '20px' }}>
-        <FormControl fullWidth margin="normal">
-          <InputLabel>Filialın Adı</InputLabel>
-          <Select
-            value={branchName}
-            onChange={(e) => setBranchName(e.target.value)}
-            label="Filialın Adı"
-            required
-          >
-            {branches.map((branch, index) => (
-              <MenuItem key={index} value={branch}>
+      <Form layout="inline" onFinish={handleSubmit} style={{ marginBottom: '20px' }}>
+        <Form.Item label="Filialın Adı" required>
+          <Select value={branchName} onChange={setBranchName} style={{ width: 200 }}>
+            {branches.map((branch) => (
+              <Option key={branch} value={branch}>
                 {branch}
-              </MenuItem>
+              </Option>
             ))}
           </Select>
-        </FormControl>
-
-        <FormControl fullWidth margin="normal">
-          <InputLabel>Kateqoriya Seçin</InputLabel>
-          <Select
-            value={selectedCategory}
-            onChange={(e) => {
-              setSelectedCategory(e.target.value);
-              setSelectedProduct(''); // Məhsul seçimini sıfırlayın
-            }}
-            required
-          >
+        </Form.Item>
+        <Form.Item label="Kateqoriya Seçin" required>
+          <Select value={selectedCategory} onChange={(value) => { setSelectedCategory(value); setSelectedProduct(''); }} style={{ width: 200 }}>
             {Object.keys(categoryProducts).map((category) => (
-              <MenuItem key={category} value={category}>
+              <Option key={category} value={category}>
                 {category}
-              </MenuItem>
+              </Option>
             ))}
           </Select>
-        </FormControl>
-
-        <FormControl fullWidth margin="normal" disabled={!selectedCategory}>
-          <InputLabel>Məhsulun Adı</InputLabel>
-          <Select
-            value={selectedProduct}
-            onChange={(e) => setSelectedProduct(e.target.value)}
-            required
-          >
+        </Form.Item>
+        <Form.Item label="Məhsulun Adı" required>
+          <Select value={selectedProduct} onChange={setSelectedProduct} style={{ width: 200 }}>
             {categoryProducts[selectedCategory]?.map((product) => (
-              <MenuItem key={product} value={product}>
+              <Option key={product} value={product}>
                 {product}
-              </MenuItem>
+              </Option>
             ))}
           </Select>
-        </FormControl>
+        </Form.Item>
+        <Form.Item>
+          <Input type="number" placeholder="Satış Miqdarı" value={soldQuantity} onChange={(e) => setSoldQuantity(e.target.value)} required />
+        </Form.Item>
+        <Form.Item>
+          <Input type="number" placeholder="Hazırlanan Miqdar" value={preparedQuantity} onChange={(e) => setPreparedQuantity(e.target.value)} required />
+        </Form.Item>
+        <Form.Item>
+          <Input type="number" placeholder="Yararsız" value={unfitQuantity} onChange={(e) => setUnfitQuantity(e.target.value)} required />
+        </Form.Item>
+        <Form.Item>
+          <Input type="number" placeholder="Bitmiş Miqdar" value={expiredQuantity} onChange={(e) => setExpiredQuantity(e.target.value)} required />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Əlavə et
+          </Button>
+        </Form.Item>
+      </Form>
 
-        <TextField
-          label="Satış Miqdarı"
-          type='number'
-          value={soldQuantity}
-          onChange={(e) => setSoldQuantity(e.target.value)}
-          required
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Hazırlanan Miqdar"
-          type='number'
-          value={preparedQuantity}
-          onChange={(e) => setPreparedQuantity(e.target.value)}
-          required
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Yararsız"
-          type='number'
-          value={unfitQuantity}
-          onChange={(e) => setUnfitQuantity(e.target.value)}
-          required
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          label="Satış Tarixi Bitmiş"
-          type='number'
-          value={expiredQuantity}
-          onChange={(e) => setExpiredQuantity(e.target.value)}
-          required
-          fullWidth
-          margin="normal"
-        />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Əlavə Et!
-        </Button>
-      </form>
-
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Filialın Adı</TableCell>
-              <TableCell>Məhsulun Adı</TableCell>
-              <TableCell>Satış Miqdarı</TableCell>
-              <TableCell>Hazırlanan Miqdar</TableCell>
-              <TableCell>Yararsız</TableCell>
-              <TableCell>Satış Tarixi Bitmiş</TableCell>
-              <TableCell>Qalıq</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.map((product, index) => (
-              <TableRow key={index}>
-                <TableCell>{product.branchName}</TableCell>
-                <TableCell>{product.productName}</TableCell>
-                <TableCell>{product.soldQuantity}</TableCell>
-                <TableCell>{product.preparedQuantity}</TableCell>
-                <TableCell>{product.unfitQuantity}</TableCell>
-                <TableCell>{product.expiredQuantity}</TableCell>
-                <TableCell>{calculateDifference(product)}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Input.Search placeholder="Axtarış..." value={searchText} onChange={handleSearch} style={{ marginBottom: '20px', width: '300px' }} />
+      <Table dataSource={filteredProducts} columns={columns} rowKey="_id" pagination={{ pageSize: 10 }} />
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store'; // Store'u import edin
 import './index.css';
+import 'antd/dist/reset.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
